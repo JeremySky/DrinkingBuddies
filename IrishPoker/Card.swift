da1b7226b237edeb88105663834057616779c477
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-11 14:45:51 -0800

    created card object

---
+++ IrishPoker/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  IrishPoker
+//
+//  Created by Jeremy Manlangit on 11/11/23.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Card()
+}
