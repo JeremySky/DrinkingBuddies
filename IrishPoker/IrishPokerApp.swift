d9d62d74f121670fb65deb5d678510e7b43a3803
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-11 10:46:33 -0800

    Initial Commit

---
+++ IrishPoker/IrishPokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrishPokerApp.swift
+//  IrishPoker
+//
+//  Created by Jeremy Manlangit on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IrishPokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
