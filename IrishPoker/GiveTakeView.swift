c7571b5ec531c4249391dea95351926cbadd8828
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-13 22:43:48 -0800

    Create GiveTakeView

---
+++ IrishPoker/GiveTakeView.swift
@@ -0,0 +1,18 @@
+//
+//  GiveTakeView.swift
+//  IrishPoker
+//
+//  Created by Jeremy Manlangit on 11/13/23.
+//
+
+import SwiftUI
+
+struct GiveTakeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GiveTakeView()
+}
