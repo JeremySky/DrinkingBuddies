518de78c505e04b37e9f1482780bb0fb6d0dc932
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-13 23:22:06 -0800

    Create WaitView

---
+++ IrishPoker/WaitView.swift
@@ -0,0 +1,18 @@
+//
+//  WaitView.swift
+//  IrishPoker
+//
+//  Created by Jeremy Manlangit on 11/13/23.
+//
+
+import SwiftUI
+
+struct WaitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaitView()
+}
