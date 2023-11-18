73fc09734263b01b810619cfca3780d62b02b001
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-18 15:09:11 -0800

    Fix bugs pertaining to game flow

---
+++ IrishPoker/SubView/AnswerButtons.swift
@@ -0,0 +1,18 @@
+//
+//  AnswerButton.swift
+//  IrishPoker
+//
+//  Created by Jeremy Manlangit on 11/17/23.
+//
+
+import SwiftUI
+
+struct AnswerButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnswerButton()
+}
