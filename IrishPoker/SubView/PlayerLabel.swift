c69c94dcae2f3bb77959e8361fab3edd0d10a193
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2023-11-27 16:09:34 -0800

    Game logic from Start of game -> Results finished

---
+++ IrishPoker/SubView/PlayerLabel.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerLabel.swift
+//  IrishPoker
+//
+//  Created by Jeremy Manlangit on 11/27/23.
+//
+
+import SwiftUI
+
+struct PlayerLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerLabel()
+}
---
+++ IrishPoker/View/PlayerResultsView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerResults.swift
+//  IrishPoker
+//
+//  Created by Jeremy Manlangit on 11/23/23.
+//
+
+import SwiftUI
+
+struct PlayerResults: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerResults()
+}
