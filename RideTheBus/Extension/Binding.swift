7e783a503d2ee0885c67a891be922d0c50bdfe12
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2024-01-11 13:34:34 -0800

    Fix continue button in results view

---
+++ RideTheBus/Extension/Binding.swift
@@ -0,0 +1,8 @@
+//
+//  Binding.swift
+//  RideTheBus
+//
+//  Created by Jeremy Manlangit on 1/5/24.
+//
+
+import Foundation
---
+++ RideTheBus/Model/Errors.swift
@@ -0,0 +1,8 @@
+//
+//  Errors.swift
+//  RideTheBus
+//
+//  Created by Jeremy Manlangit on 1/5/24.
+//
+
+import Foundation
